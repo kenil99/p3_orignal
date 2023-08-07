8b0818391225160338ae82c56155dcaf731964b0
student <student@gnu.ac.in>
2023-08-07 10:45:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_p3_33/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.mad_p3_33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
